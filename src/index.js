9dbfb74941ef6553ce7eb1c10836799017576446
Jeremy Brown <jlbmagic@gmail.com>
2022-02-23 15:16:58 -0500

    installed Apex and set up demo chart

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 //Here we're importing items we'll need. You can add other imports here.
-
+import ApexCharts from 'apexcharts'
 //The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
 
-//
+var options = {
+  chart: {
+    type: 'line'
+  },
+  series: [{
+    name: 'sales',
+    data: [30,40,35,50,49,60,70,91,125]
+  }],
+  xaxis: {
+    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+  }
+}
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
