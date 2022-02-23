a3f4c877559f0ca86a7aefca7662b42df459e4ec
Jeremy Brown <jlbmagic@gmail.com>
2022-02-23 15:19:36 -0500

    added 2nd chart with options

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 //Here we're importing items we'll need. You can add other imports here.
-import ApexCharts from 'apexcharts'
+import ApexCharts from "apexcharts";
 //The first function. Remove this.
 
 var options = {
   chart: {
-    type: 'line'
+    type: "line",
   },
-  series: [{
-    name: 'sales',
-    data: [30,40,35,50,49,60,70,91,125]
-  }],
+  series: [
+    {
+      name: "sales",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+  ],
   xaxis: {
-    categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-  }
-}
-
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+};
+var optionsTwo = {
+  chart: {
+    type: "bar",
+  },
+  series: [
+    {
+      name: "sales",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+  ],
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+};
 var chart = new ApexCharts(document.querySelector("#chart"), options);
+var chartTwo = new ApexCharts(document.querySelector("#chartTwo"), optionsTwo);
 
 chart.render();
+chartTwo.render();
